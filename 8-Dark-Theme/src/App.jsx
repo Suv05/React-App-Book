932f9b08dc8ae77e974801bc0b9c84f3138e8efe
suvam <suvampravu@gmail.com>
2024-06-26 13:04:43 +0530

    Day-8 project-9 finished

--- 8-Dark-Theme/src/App.jsx
+++ 8-Dark-Theme/src/App.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
+import useLocalStorage from "./Components/useLocalStorage";
+
 function App() {
+  const [theme, setTheme] = useLocalStorage("theme", "dark");
+
+  const handleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
   return (
     <>
-      <div className="container mx-5 my-5">
-        <h1 className="d-flex">Hello World !</h1>
-        <button className="btn btn-outline-warning btn-lg mt-4 mx-4 ">
-          Change Theme
-        </button>
+      <div className="light-dark-theme" data-theme={theme}>
+        <div className="container mx-5 my-5">
+          <h1 className="d-flex">Hello World!</h1>
+          <button
+            className="btn btn-outline-warning btn-lg mt-4 mx-4"
+            onClick={handleTheme}
+          >
+            Change Theme
+          </button>
+        </div>
       </div>
     </>
   );
--- 8-Dark-Theme/src/Components/useLocalStorage.jsx
+++ 8-Dark-Theme/src/Components/useLocalStorage.jsx
@@ -1,16 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-function useLocalStorage({ key, defaultValue }) {
+function useLocalStorage(key, defaultValue) {
   const [value, setValue] = useState(() => {
     let currentValue;
     try {
-        
+      currentValue = JSON.parse(localStorage.getItem(key)) || defaultValue;
     } catch (error) {
       console.log(error);
       currentValue = defaultValue;
     }
+    return currentValue;
   });
-  return currentValue;
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [value, key]);
+
+  return [value, setValue];
 }
 
 export default useLocalStorage;
