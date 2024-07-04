144ce67387a7129b7e763d73a52b211df55e8999
suvam <suvampravu@gmail.com>
2024-07-04 21:30:15 +0530

    Pro-17 finished

--- 17-Scroll-Top-Bottom/src/App.jsx
+++ 17-Scroll-Top-Bottom/src/App.jsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState, useEffect } from "react";
+import { FaAnglesDown, FaAnglesUp } from "react-icons/fa6";
 function App() {
-  const [count, setCount] = useState(0)
+  const url = "https://dummyjson.com/products?limit=0";
+  const [loading, setLoading] = useState(false);
+  const [items, setItems] = useState([]);
+
+  const scrollTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+  const scrollBottom = () => {
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: "smooth",
+    });
+  };
+  useEffect(() => {
+    let fetchProducts = async () => {
+      try {
+        setLoading(true);
+        let response = await fetch(url);
+        let data = await response.json();
+        setItems(data.products);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+        setLoading(false);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="text-center mt-2">
+        <b>Loading....</b>
+      </div>
+    );
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="container text-center">
+        <h1 style={{ textDecoration: "underline" }}>Custom Products</h1>
+        <button
+          type="button"
+          class="btn btn-primary btn-sm my-3"
+          onClick={scrollBottom}
+        >
+          <FaAnglesDown size={30} />
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        {items && items.length
+          ? items.map((item, index) => (
+              <p key={index}>
+                <strong>{item.title}</strong>
+              </p>
+            ))
+          : null}
+        <button
+          type="button"
+          class="btn btn-primary btn-sm my-3"
+          onClick={scrollTop}
+        >
+          <FaAnglesUp size={30} />
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
