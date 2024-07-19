87fd8839fab8d1be6bf3c75dc2e8a1beb98f1945
suvam <suvampravu@gmail.com>
2024-07-19 10:47:25 +0530

    commit changes

---
+++ 20-Food-Recipe-App/src/components/routes/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Footer({}) {
+  return <div>Footer Content</div>;
+}
+
+export default Footer;
---
+++ 20-Food-Recipe-App/src/components/routes/Header.jsx
@@ -0,0 +1,11 @@
+
+
+function Header({}) {
+  return (
+    <div>
+     Header Content
+    </div>
+  );
+}
+
+export default Header;
