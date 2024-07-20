98fa2afefa5924ab627d9c0d48bd86994d36c674
suvam <suvampravu@gmail.com>
2024-07-20 18:39:22 +0530

    commit changes

--- 20-Food-Recipe-App/src/main.jsx
+++ 20-Food-Recipe-App/src/main.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/routes/App";
-import "./index.css";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   RouterProvider,
   Route,
 } from "react-router-dom";
+import "./index.css";
+import App from "./components/routes/App";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<App />}></Route>)
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      
+    </Route>
+  )
 );
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
