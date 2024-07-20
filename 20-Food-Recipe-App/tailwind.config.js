fff86f9400cc0dcb749b8df905e0284a1fc15a97
suvam <suvampravu@gmail.com>
2024-07-20 18:39:11 +0530

    commit changes

--- 20-Food-Recipe-App/tailwind.config.js
+++ 20-Food-Recipe-App/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: "#fda218"
+      },
+      fontFamily: {
+        "noto-sans": ['"Noto Sans"', "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
