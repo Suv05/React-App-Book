0a2cb558f50f8b9dd33334ecead7df63680365b6
suvam <suvampravu@gmail.com>
2024-07-24 10:28:15 +0530

    commit the spinner component

--- 20-Food-Recipe-App/tailwind.config.js
+++ 20-Food-Recipe-App/tailwind.config.js
@@ -13,6 +13,15 @@ export default {
         "hero-img":
           "url('https://media.istockphoto.com/id/1436956975/photo/chef-pouring-special-sauce-on-pork-ribs-in-the-kitchen.webp?b=1&s=170667a&w=0&k=20&c=dqjedk3BRtemi6aGrR62j4_HQ7NVZIsGNyTHOedV4_o=')",
       },
+      animation: {
+        spin: "spin 1s linear infinite",
+      },
+      keyframes: {
+        spin: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
+      },
     },
   },
   plugins: [],
